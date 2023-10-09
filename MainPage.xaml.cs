4717ceee4bd256dc10f16a1792f8417cfc4a962d
demar27 <126399077+demar27@users.noreply.github.com>
2023-10-09 09:19:00 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void OnDial(object sender, EventArgs e)
+	{
+		try
+		{
+            PhoneDialer.Open(PhoneNum.Text);
+
+        }
+        catch (Exception ex)
+		{
+            await DisplayAlert("Invalid input", "Please enter a valid phone number", "OK");
+			
+        }
+    }
+
+}
+
